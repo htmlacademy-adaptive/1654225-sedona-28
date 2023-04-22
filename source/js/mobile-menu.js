ea41f9ba67e5c72d10620afcacf9a1942d7c07ab
Nadezhda <nadezda.andreeva.94@mail.ru>
2023-04-22 14:48:24 +0300

    Декоративные элементы

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@ const openMenuLink = document.querySelector(".main-nav__toggle");
 const menu = document.querySelector(".main-nav");
 //const modalClose = searchModal.querySelector(".modal-close-button");
 
+menu.classList.remove('main-nav--nojs');
+
 openMenuLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   menu.classList.toggle("main-nav--open");
