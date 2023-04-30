cd49624955693c8e1657641f363b9933a55fc89e
Keks <keks@htmlacademy.ru>
2023-04-30 22:02:42 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/map.js
@@ -0,0 +1,6 @@
+const mapImage = document.querySelector(".map__image");
+const mapFrame = document.querySelector(".map__frame");
+
+
+mapImage.classList.remove('map__image--nojs');
+mapFrame.classList.remove('map__frame--nojs');
