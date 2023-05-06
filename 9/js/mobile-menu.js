680cbacde2bbe5ac2f6bab926282954a7ef7399d
Keks <keks@htmlacademy.ru>
2023-05-06 15:17:55 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/mobile-menu.js
@@ -0,0 +1,1 @@
+const openMenuLink=document.querySelector(".main-nav__toggle"),menu=document.querySelector(".main-nav");menu.classList.remove("main-nav--nojs"),openMenuLink.addEventListener("click",(function(n){n.preventDefault(),menu.classList.toggle("main-nav--open"),menu.classList.toggle("main-nav--closed")}));
